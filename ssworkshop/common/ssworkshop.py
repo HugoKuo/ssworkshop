e28abac84560861a7f6a272ae74b4cbee8783255
HugoKuo <tonytkdk@gmail.com>
2014-06-17 19:34:00 +0800

    normal update

---
+++ ssworkshop/common/ssworkshop.py
@@ -0,0 +1,37 @@
+
+from argparse import ArgumentParser
+
+
+
+def run(args=None):
+    parsed_args = parse_args(args=args)
+    return "123"
+
+
+def parse_args(args=None):
+    parser = ArgumentParser(
+        description='A tool to manage devices in a swift cluster')
+    subparsers = parser.add_subparsers(help='commands for sdt')
+
+    pass
+
+
+
+#Class Connection():
+
+
+
+
+
+#def list_images():
+
+
+#def list
+
+
+#List images
+
+
+#List instance
+
+#
